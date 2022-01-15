8c6c5a4a5c16db62c2ca0a4dbbce26977cedc64f
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-01-16 01:29:52 +0500

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -5,20 +5,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Register from './Register';
 import Login from './Login';
 import AfterLogin from './AfterLogin';
-import Profile from './Profile';
-
+import Profile from "./Profile"
 
 const Stack = createStackNavigator();
 
 export default function Navigator(props) {
     return (
         <NavigationContainer>
-            <Stack.Navigator  screenOptions={{ headerStyle: { backgroundColor: 'black' } }} >
+            <Stack.Navigator >
             <Stack.Screen name="Login" component={Login} options={{ title:"Login" }} />
             <Stack.Screen name="Register" component={Register} options={{ title:"Register"}} />
             <Stack.Screen name="AfterLogin" component={AfterLogin} options={{ title:"AfterLogin"}} />
             <Stack.Screen name="Profile" component={Profile} options={{ title:"Profile"}} />
-
 
             </Stack.Navigator>
         </NavigationContainer>
