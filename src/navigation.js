758739e10d9b332c1b7d2121145ef3eecbeeee4b
Aleezah Ikram <64039135+Aleezah1429@users.noreply.github.com>
2022-01-15 12:48:03 +0500

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -5,9 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Register from './Register';
 import Login from './Login';
 import AfterLogin from './AfterLogin';
-import Profile from './Profile'
-import ConnectionWithBLE from "./ConnectionWithBLE"
-import Oximeter from "./Oximeter"
+import Profile from './Profile';
 
 
 const Stack = createStackNavigator();
@@ -16,12 +14,10 @@ export default function Navigator(props) {
     return (
         <NavigationContainer>
             <Stack.Navigator  screenOptions={{ headerStyle: { backgroundColor: 'black' } }} >
-            {/* <Stack.Screen name="Login" component={Login} options={{ title:"Login" }} /> */}
-            {/* <Stack.Screen name="Register" component={Register} options={{ title:"Register"}} /> */}
+            <Stack.Screen name="Login" component={Login} options={{ title:"Login" }} />
+            <Stack.Screen name="Register" component={Register} options={{ title:"Register"}} />
             <Stack.Screen name="AfterLogin" component={AfterLogin} options={{ title:"AfterLogin"}} />
-            {/* <Stack.Screen name="Profile" component={Profile} options={{ title:"Profile"}} /> */}
-            {/* <Stack.Screen name="ConnectionWithBLE" component={ConnectionWithBLE} options={{ title:"ConnectionWithBLE"}} /> */}
-            {/* <Stack.Screen name="Oximeter" component={Oximeter} options={{ title:"Oximeter"}} /> */}
+            <Stack.Screen name="Profile" component={Profile} options={{ title:"Profile"}} />
 
 
             </Stack.Navigator>
